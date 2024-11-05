604a35a1e8d117596eda25b1c380d6e18bc8acf8
N-SilverJr <s.nshuti@alustudent.com>
2024-11-05 23:14:45 +0200

    Print Tebahpla

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# This program prints the ASCII alphabet in reverse order, alternating lowercase and uppercase
+
+print("".join("{}".format(chr(c) if c % 2 == 0 else chr(c).upper()) for c in range(122, 96, -1)), end="")
